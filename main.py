dbb37bb52faa2f4a8670c4630e3af5694db7b1f8
Smuzi27 <artur.grebennik@gmail.com>
2024-02-27 19:23:59 +1000

    commit camera

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
+import math
+import random as rnd
 
 WIDTH, HEIGHT = 1200, 800  # Размеры игрового окна
-WIDTH_ZONE, HEIGHT_ZONE = 3000, 3000  # Размеры игрового поля
-FPS = 60  # Фиксация количества кадров в секунду
+WIDTH_ZONE, HEIGHT_ZONE = 1200, 800  # Размеры игрового поля
+FPS = 60  # Количествo кадров в секунду
 
 # Инициализирование pygame
 pygame.init()
@@ -16,7 +18,7 @@ class Painter:
     # Класс, который объединяет все объекты и отрисовывает их
 
     def __init__(self):
-        self.paints = []  # Cписок объектов
+        self.paints = []  # Список объектов
 
     def add(self, can_paint):
         # Добавляет объект
@@ -39,9 +41,13 @@ class Game:
 
     def start(self):
         # Запускает игру
+
+        global camera
         self.is_running = True
         painter = Painter()
         painter.add(grid)
+        painter.add(bacterium)
+
         while self.is_running:
             clock.tick(FPS)
             events = pygame.event.get()
@@ -56,28 +62,118 @@ class Game:
                     pygame.quit()
                     quit()
 
+            bacterium.move()
+
+            if camera is not None:
+                camera.update(bacterium)
+
             screen.fill((9, 10, 21))
             painter.paint()
 
             pygame.display.flip()
 
 
-class Grid:
+class CanPaint:
+    # Класс объектов которые отрисовываются на экране
+
+    def __init__(self, surface, cam):
+        self.surface = surface
+        self.camera = cam
+
+
+class Camera:
+    # Класс камеры
+
+    def __init__(self):
+        self.x, self.y = 0, 0
+        self.width, self.height = WIDTH, HEIGHT
+        self.zoom = 0.5
+
+    def centre(self, bacterium_or_position):
+        # Центрирует камеру по игроку
+        if isinstance(bacterium_or_position, Player):
+            x, y = bacterium_or_position.x, bacterium_or_position.y
+            self.x = (WIDTH / 2) - (x * self.zoom)
+            self.y = (HEIGHT / 2) - (y * self.zoom)
+        elif bacterium_or_position is tuple:
+            self.x, self.y = bacterium_or_position
+
+    def update(self, target):
+        # Обновляет зум камеры, зависящий от набранной массы игрока
+        self.zoom = 100 / target.mass + 0.3
+        self.centre(bacterium)
+
+
+class Grid(CanPaint):
     # Класс сетки игры
 
-    def __init__(self, surface):
-        self.surface = surface
+    def __init__(self, surface, cam):
+        super().__init__(surface, cam)
         self.color_grid = "#00bfff"
 
     def draw(self):
         # Рисование сетки на экране
+        zoom = self.camera.zoom
+        x, y = self.camera.x, self.camera.y
         for i in range(0, WIDTH_ZONE + 1, 50):
-            pygame.draw.line(self.surface, self.color_grid, (0, i), (WIDTH_ZONE, i), 3)
-            pygame.draw.line(self.surface, self.color_grid, (i, 0), (i, WIDTH_ZONE), 3)
+            pygame.draw.line(self.surface, self.color_grid, (x, y + i * zoom), (WIDTH_ZONE * zoom + x, i * zoom + y), 3)
+            pygame.draw.line(self.surface, self.color_grid, (x + i * zoom, y), (i * zoom + x, WIDTH_ZONE * zoom + y), 3)
+
+
+class Player(CanPaint):
+    # Класс игрока
+
+    FONT_COLOR = "#deeece"  # Цвет имени и массы игрового персонажа
+
+    def __init__(self, surface, cam):
+        super().__init__(surface, cam)
+        self.x, self.y = rnd.randint(300, WIDTH_ZONE - 300), rnd.randint(300, WIDTH_ZONE - 300)
+        self.mass, self.speed = 20, 4
+        self.outline_size = 3 + self.mass / 2
+        self.color = "#00c77b"
+        self.outline_color = "#007a3f"
+        self.poison = False
+
+    def move(self):
+        # Движение игрока
+        self.outline_size = 3 + self.mass / 2
+        go_x, go_y = pygame.mouse.get_pos()
+        rotation = math.atan2(go_y - float(HEIGHT) / 2, go_x - float(WIDTH) / 2)
+        rotation *= 180 / math.pi
+        normalized = (90 - abs(rotation)) / 90
+        vx = self.speed * normalized
+        if rotation < 0:
+            vy = -self.speed + abs(vx)
+        else:
+            vy = self.speed - abs(vx)
+
+        if self.x + vx - self.outline_size < 0:
+            self.x = self.outline_size
+        elif self.x + vx + self.outline_size > WIDTH_ZONE:
+            self.x = WIDTH_ZONE - self.outline_size
+        else:
+            self.x += vx
+
+        if self.y + vy - self.outline_size < 0:
+            self.y = self.outline_size
+        elif self.y + vy + self.outline_size > HEIGHT_ZONE:
+            self.y = HEIGHT_ZONE - self.outline_size
+        else:
+            self.y += vy
+
+    def draw(self):
+        # Рисует игрока
+        zoom = self.camera.zoom
+        x, y = self.camera.x, self.camera.y
+        center = (int(self.x * zoom + x), int(self.y * zoom + y))
+
+        pygame.draw.circle(self.surface, self.outline_color, center, int((self.mass / 2 + 3) * zoom))
+        pygame.draw.circle(self.surface, self.color, center, int(self.mass / 2 * zoom))
 
 
 if __name__ == '__main__':
-    grid = Grid(screen)
-
+    camera = Camera()
+    grid = Grid(screen, camera)
+    bacterium = Player(screen, camera)
     game = Game()
     game.start()
